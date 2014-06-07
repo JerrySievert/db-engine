ef1b67f4bd5a314f0b2eb88888c0d77a8472126f
Jerry Sievert <code@legitimatesounding.com>
2014-06-07 10:24:05 -0700

    refactor to allow for multiple index stores

--- test/index_test.js
+++ test/index_test.js
@@ -1,45 +1,50 @@
 var vows = require('vows'),
     assert = require('assert');
 
-var index = require('../lib/index');
+var indexStore = require('../lib/index_store');
 var testIndex = require('./helpers/simple_index').SimpleIndex;
 var testStore = require('./helpers/simple_store').SimpleStore;
 
 vows.describe('index.js').addBatch({
   "When an index is added": {
     topic: function ( ) {
-      index.addIndex({ property: "test", index: new testIndex() }, this.callback);
+      this.index = new indexStore();
+      this.index.addIndex({ property: "test", index: new testIndex() }, this.callback);
     },
     "the index count is one": function ( ) {
-      assert.equal(index.indexes.length, 1);
+      assert.equal(this.index.indexes.length, 1);
     }
   },
   "When data is added to an index": {
     topic: function ( ) {
-      index.add(1, { test: "foo" }, this.callback);
+      this.index = new indexStore();
+      var self = this;
+      this.index.addIndex({ property: "test", index: new testIndex() }, function () {
+        self.index.add(1, { test: "foo" }, self.callback);
+      });
     },
     "the index will contain the entry": function ( ) {
-      assert.equal(Object.keys(index.indexes[0].index.entries).length, 1);
-      assert.equal(index.indexes[0].index.entries.foo[0], 1);
+      assert.equal(Object.keys(this.index.indexes[0].index.entries).length, 1);
+      assert.equal(this.index.indexes[0].index.entries.foo[0], 1);
     },
     "When data is removed from an index": {
       topic: function ( ) {
-        index.remove(1, { test: "foo" }, this.callback);
+        this.index.remove(1, { test: "foo" }, this.callback);
       },
       "the index will no longer contain the entry": function ( ) {
-        assert.equal(Object.keys(index.indexes[0].index.entries).length, 0);
+        assert.equal(Object.keys(this.index.indexes[0].index.entries).length, 0);
       }
     },
     "When data is already there and an index is added": {
       topic: function ( ) {
         var store = new testStore();
-        index.setBackingStore(store);
+        this.index.setBackingStore(store);
         store.add(1, { foo: "bar" });
-        index.add(1, { foo: "bar" }, function ( ) { });
-        index.addIndex({ property: "foo", index: new testIndex() }, this.callback);
+        this.index.add(1, { foo: "bar" }, function ( ) { });
+        this.index.addIndex({ property: "foo", index: new testIndex() }, this.callback);
       },
       "the new index will contain the entry": function ( ) {
-        assert.equal(Object.keys(index.indexes[1].index.entries).length, 1);
+        assert.equal(Object.keys(this.index.indexes[1].index.entries).length, 1);
       }
     }
   },
@@ -47,12 +52,32 @@ vows.describe('index.js').addBatch({
     topic: function ( ) {
       var simple = new testIndex();
 
+      var index = new indexStore();
       simple.add("foo", 1);
       index.searchIndex(simple, 'foo', 'equals', this.callback);
     }, "the correct results should be returned": function (err, results, topic) {
       assert.equal(results.length, 1);
       assert.equal(topic.indexType, "SimpleIndex");
       assert.equal(topic.resultsFound, 1);
+    }
+  },
+  "When searching multiple indexes": {
+    topic: function ( ) {
+      var self = this;
+      var index = new indexStore();
+
+      index.addIndex({ property: "foo", index: new testIndex() }, function ( ) {
+        index.addIndex({ property: "foo", index: new testIndex() }, function ( ) {
+          index.add(1, { "foo": "bar" }, function ( ) {
+            index.add(2, { "foo": "baz" }, function ( ) {
+              index.searchIndexes({ "key": "foo", "value": "bar", "type": "equals" }, self.callback);
+            });
+          });
+        });
+      });
+    },
+    "the correct entry is returned": function (err, topic, metrics) {
+      assert.equal(topic[0], 1);
     }
   }
 }).export(module);
